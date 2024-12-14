be5a3f8c05cde0fb1abe980a5ee3430ebf87dece
Daniel <danielmcolares@gmail.com>
2024-12-14 12:17:34 -0300

    feat: basic theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,23 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#000000",
+      error: "#FC311D",
+      primary: {
+        500: "#00A19D",
+      },
+      gray: {
+        200: "#F4F4F4",
+        300: "#F3F1F1",
+        600: "#C4C4C4",
+        700: "#B0B0B0",
+        800: "#979797",
+      },
+    },
     extend: {
       colors: {
         background: "var(--background)",
