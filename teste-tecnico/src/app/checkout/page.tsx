b87ab9516594086a4979287f4ce361ae66806aca
marcos <mvcoladello@hotmail.com>
2024-12-17 22:14:03 -0300

    add header

--- teste-tecnico/src/app/checkout/page.tsx
+++ teste-tecnico/src/app/checkout/page.tsx
@@ -1,13 +1,18 @@
 import Image from 'next/image';
-import { roboto700 } from './ui/fonts';
+import { roboto700 } from '@/app/ui/fonts';
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center h-screen p-8'>
       <header className='flex flex-row items-center gap-3'>
         <Image src='/CarbonLogo.svg'  width={30} height={45} alt="Logo" />
         <h1 className={`text-4xl text-primaria ${roboto700.className}`}>Checkout Carbon</h1>
       </header>
+      <main className='flex flex-row h-full'>
+        <div className='w-2/3 flex flex-col items-start justify-start'>
+          {/*formulario */}
+        </div>
+      </main>
     </div>
   );
 }
