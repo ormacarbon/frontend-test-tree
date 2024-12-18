38bf10169ad219349c7ebb69d43d768cd4f21a95
Marcos Paulo <marcospaulosousa48@gmail.com>
2024-12-18 11:28:51 -0300

    feat: finalize checkout pages

--- app/checkout-error/page.tsx
+++ app/checkout-error/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Header } from "../_components/header";
 import Image from "next/image";
+import Link from "next/link";
 const CheckoutSuccessPage = () => {
   return (
     <main className="mx-auto flex w-full max-w-[1080px] flex-1 flex-col justify-center gap-8 py-10">
@@ -11,27 +12,26 @@ const CheckoutSuccessPage = () => {
         }
       >
         <section className="flex w-full flex-1 flex-col items-center justify-start gap-4 px-[31px] py-3 lg:mt-16 lg:items-start lg:justify-normal lg:gap-9">
-          <h3 className="text-center text-3xl font-bold text-primary lg:text-5xl">
-            Parabéns!
+          <h3 className="text-center text-3xl font-bold text-[#FC311D] lg:text-5xl">
+            Desculpe
           </h3>
 
-          <h6 className="whitespace-pre text-center text-xl font-bold text-primary lg:text-left lg:text-2xl">
-            Você compensou suas emissões {"\n"}com sucesso!
+          <h6 className="whitespace-pre text-center text-xl font-bold text-[#FC311D] lg:text-left lg:text-2xl">
+            Ocorreu um erro inesperado
           </h6>
           <h4 className="text-md font-roboto-condesend-medium text-center lg:text-left lg:text-lg">
+            <p className="pb-2">Verifique as informações do seu cartão</p>
             <p className="whitespace-pre">
-              Com essa ação você contribui para um mundo mais {"\n"}sustentável
-            </p>
-            <p className="whitespace-pre">
-              Um email com o certificado de compensação será {"\n"}enviado para
-              você em breve
+              Se o problema persiste, contate-se com o seu {"\n"} banco
             </p>
           </h4>
 
           <div className="flex w-full flex-row justify-center p-12 lg:justify-normal lg:p-0">
-            <Button className="max-w-[285px] border-primary bg-primary font-bold">
-              Compartilhar
-            </Button>
+            <Link href="/">
+              <Button className="max-w-[285px] border-primary bg-primary font-bold">
+                Tente novamente
+              </Button>
+            </Link>
           </div>
         </section>
         <section className="flex flex-row">
