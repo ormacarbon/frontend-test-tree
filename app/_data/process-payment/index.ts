b4af48a44662d903c90c3f71648cea2eff952eff
Marcos Paulo <marcospaulosousa48@gmail.com>
2024-12-18 11:30:41 -0300

    feat: creation of routes

---
+++ app/_data/process-payment/index.ts
@@ -0,0 +1,21 @@
+import { Payment } from "../../_types/payment";
+import { redirect } from "next/navigation";
+export const processPayment = async (data: Payment) => {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API}/passyment`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      redirect("/checkout-error");
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Erro ao processar pagamento:", error);
+    throw error;
+  }
+};
