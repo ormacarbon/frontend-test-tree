4149a92586d322dcfc61a1468eaba85b24fb6f56
Marcos Paulo <marcospaulosousa48@gmail.com>
2024-12-18 16:39:21 -0300

    fix: url to api

--- app/_data/process-payment/index.ts
+++ app/_data/process-payment/index.ts
@@ -2,7 +2,7 @@ import { Payment } from "../../_types/payment";
 import { redirect } from "next/navigation";
 export const processPayment = async (data: Payment) => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API}/passyment`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API}/payment`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
