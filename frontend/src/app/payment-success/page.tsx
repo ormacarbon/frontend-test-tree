be2543e219348165efc4cdcf1f72e59d8418b3ce
aldacelio <aldacelio@alu.ufc.br>
2025-06-06 17:05:03 -0300

    feat: adding success and error page

---
+++ frontend/src/app/payment-success/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function PaymentSuccessPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Sucesso</h1>
+    </div>
+  );
+}
