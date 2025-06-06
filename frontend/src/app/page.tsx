1ab3a80cbb20d110448fc12d27b40cba3357779a
aldacelio <aldacelio@alu.ufc.br>
2025-06-06 14:27:05 -0300

    feat: adding button and redirect route to checkout page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,27 @@
-export default function Home() {
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export default function HomePage() {
+  const router = useRouter();
+
+  const handleStartCheckout = () => {
+    router.push("/checkout?co2=1&cred=2");
+  };
+
   return (
-    <main className="min-h-screen bg-gray-50 flex items-center justify-center p-8">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-gray-900 mb-4">
-          Hello World
-        </h1>
-      </div>
-    </main>
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-4 gap-8">
+      <h1 className="text-3xl font-bold text-center text-gray-800">
+        Clique no botão para ir a página solicitada
+      </h1>
+      <Button
+        onClick={handleStartCheckout}
+        className="w-[300px] bg-primary hover:bg-primary"
+        size="lg"
+      >
+        Ir para o checkout
+      </Button>
+    </div>
   );
 }
